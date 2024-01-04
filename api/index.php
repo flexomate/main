46d1c3cdad7d8c31638e036776b37d32e28f6292
Kaushal Krishna <132030841+kaushal-krishna@users.noreply.github.com>
2024-01-04 15:37:01 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,1 +1,15 @@
+<?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+$method = $_SERVER("REQUEST_METHOD");
+
+switch ($method) {
+  case "POST" :
+    $input = file_get_contents('php://input');
+    echo $input;
+    $dataFile = fopen("student.json", "w");
+    fwrite($dataFile, $input);
+    $response = ["status" => 200; "message" => "Post executed successfully!"];
+    echo($response);
+}
